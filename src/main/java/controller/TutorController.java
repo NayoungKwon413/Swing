1e085dd923e46a10a0a90cbb822a565d411d24aa
hyelyun-y <68841090+hyelyun-y@users.noreply.github.com>
2020-08-18 16:11:52 +0900

    Update TutorController.java

--- src/main/java/controller/TutorController.java
+++ src/main/java/controller/TutorController.java
@@ -46,13 +46,21 @@ public class TutorController {
 		return mav;
 	}
 	
-	// 수업 등록 
-	// 1.유저 정보 update 2. 자격증 정보 insert 3. 수업 정보 insert
-	// @RequestParam Map<String,Object> map
+	/* 수업 등록 
+	 1.유저 정보 update 2. 자격증 정보 insert 3. 수업 정보 insert
+	 @RequestParam Map<String,Object> map
+	*/
 	@PostMapping("classEntry")
-	public ModelAndView classEntry(User user, License license, Class clas) {
+	public ModelAndView classEntry(User user, License license, Class clas, HttpSession session) {
 		ModelAndView mav = new ModelAndView();
-		System.out.println(map);
+		User loginUser = (User)session.getAttribute("loginUser");
+		
+		//3. 수업 정보
+		// classid 설정
+		// userid 설정
+		// readcnt(0), state(1.등록진행중 2.승인대기), regdate(now)
+		clas.setUserid(userid);
+		service.classInsert(clas);
 		return mav;
 	}
 }
