dd4a6e3eb82ae4222c6cf08f8bef71ac9eab618e
hyelyun-y <68841090+hyelyun-y@users.noreply.github.com>
2020-08-18 16:14:46 +0900

    Update TutorController.java

--- src/main/java/controller/TutorController.java
+++ src/main/java/controller/TutorController.java
@@ -46,8 +46,20 @@ public class TutorController {
 		return mav;
 	}
 	
-	@GetMapping("register")
+	/* 수업 등록 페이지 접근시 작성 중인 수업 정보가 있는지 확인
+	 * 
+	 */
+	@RequestMapping("register")
 	public ModelAndView register(HttpSession session) {
+		ModelAndView mav = new ModelAndView();
+		User loginUser = (User)session.getAttribute("loginUser");
+		int classid = service.checkClass(loginUser.getUserid());
+		
+		if(classid > 0) {
+			
+		}else {
+			return mav;
+		}
 		
 	}
 	
@@ -59,12 +71,25 @@ public class TutorController {
 	public ModelAndView classEntry(User user, License license, Class clas, HttpSession session) {
 		ModelAndView mav = new ModelAndView();
 		User loginUser = (User)session.getAttribute("loginUser");
+		String userid = loginUser.getUserid();
 		
-		//3. 수업 정보
-		// classid 설정
-		// userid 설정
-		// readcnt(0), state(1.등록진행중 2.승인대기), regdate(now)
+		/* 1. 유저 정보 update
+		 * : 회원가입시 file 과 겹치는데 어떻게?
+		 *   학력 (edulevel) 은 뭐야?
+		 */
+		user.setUserid(userid);
+		service.userUpdate2(user);
+		
+		/* 2. 자격증 정보 insert
+		 * 
+		 */
+		
+		/* 3. 수업 정보 insert
+		 * : classid 설정, userid 설정 필요
+		 *   readcnt(0), state(1.등록진행중 2.승인대기), regdate(now) 는 쿼리에서 설정
+		 */
 		clas.setUserid(userid);
+		
 		service.classInsert(clas);
 		return mav;
 	}
